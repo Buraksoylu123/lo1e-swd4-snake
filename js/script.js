c721d504b522cf4d0264f16bfec8f5bb510f7391
buraksoylu <94455405+buraksoylu@users.noreply.github.com>
2022-09-13 13:07:23 +0200

    m

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('snake')
+let context = canvas.getContext ('2d')
+
+context.fillStyle = "green";
+context.fillRect(0, 0, 400, 400);
+
+context.fillStyle = "red";
+context.fillRect(190, 190, 20, 20);
+
