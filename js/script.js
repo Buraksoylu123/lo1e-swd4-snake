b3a3486d171fd40d8be9a73d2583c53c9324c53f
buraksoylu <94455405+buraksoylu@users.noreply.github.com>
2022-09-13 13:21:53 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let canvas = document.getElementById('snake')
 let context = canvas.getContext ('2d')
 
-context.fillStyle = "green";
+context.fillStyle = "black";
 context.fillRect(0, 0, 400, 400);
 
 context.fillStyle = "red";
 context.fillRect(190, 190, 20, 20);
+
+
+function update() {
+    console.log('update');
+}
+
+setInterval(update, 1000);
 
